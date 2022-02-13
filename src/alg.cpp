2c788766266c8b918f8a23b0753636a4a74b2356
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-02-13 20:35:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+   if ((b==0)||(a==0)){
+       return (a+b);
+   }
+   else if(b>a) {
+       if (b%a==0){
+           return (a);
+       }
+       else {
+          return (a/(b%a)); 
+       }
+
+   }
+   else if(a>b) {
+       if (a%b==0){
+           return (b);
+       }
+       else {
+          return (b/(a%b)); 
+       }
+
+   }
+    return 0;
 }
+
+   cout<<gcd(18,30);
